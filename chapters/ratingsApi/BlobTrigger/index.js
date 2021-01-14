cd9e656f9a9b1d99e3f724b2e2cf0f369bd2d7f6
Roland Nyamoga <ronyamo@microsoft.com>
2021-01-14 10:40:03 -0500

    listing all blobs

--- chapters/ratingsApi/BlobTrigger/index.js
+++ chapters/ratingsApi/BlobTrigger/index.js
@@ -1,4 +1,20 @@
+const { BlobServiceClient } = require("@azure/storage-blob");
+
+// Load the .env file if it exists
+require("dotenv").config();
+
+const STORAGE_CONNECTION_STRING = process.env.svlessbatch_STORAGE || "";
+// Note - Account connection string can only be used in node.
+const blobServiceClient = BlobServiceClient.fromConnectionString(STORAGE_CONNECTION_STRING);
+const containerClient = blobServiceClient.getContainerClient('orders');
+
 module.exports = async function (context, myBlob) {
     context.log("JavaScript blob trigger function processed blob \n Blob:", context.bindingData.blobTrigger, "\n Blob Size:", myBlob.length, "Bytes");
-    context.log(myBlob.length);
+
+    let i = 1;
+    for await (const blob of containerClient.listBlobsFlat()) {
+      console.log(`Blob ${i++}: ${blob.name}`);
+    }
+
+    context.log(myBlob);
 };
